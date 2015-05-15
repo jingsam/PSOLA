15e7ef8ff7e2fc555e2a5e9d100c4e9d0c0a2ed6
jingsam <jingsam@163.com>
2015-05-15 09:27:04 +0800

    add files

---
+++ src/init2.cc
@@ -0,0 +1,41 @@
+#include "init.h"
+#include "option.h" // g_option, g_rnd, g_land_use_map
+#include "tool.h"   // stoi(), stod()
+
+
+PlanMap* init_planmap();
+
+Particle* init_particle()
+{
+    Particle* particle = new Particle();
+    particle->current = init_planmap();
+    particle->pbest = particle->current->clone();
+
+    return particle;
+}
+
+Swarm* init_swarm(int size)
+{
+    double momentum = stod(g_option["momentum"]);
+    double c1 = stod(g_option["c1"]);
+    double c2 = stod(g_option["c2"]);
+    int r1 = stoi(g_option["r1"]);
+    int r2 = stoi(g_option["r2"]);
+
+    Swarm* swarm = new Swarm();
+    swarm->momentum = momentum;
+    swarm->c1 = c1;
+    swarm->c2 = c2;
+    swarm->r1 = new Random(r1);
+    swarm->r2 = new Random(r2);
+
+    for (int i = 0; i < size; ++i) {
+        Particle* particle = init_particle();
+        particle->swarm = swarm;
+        swarm->push_back(particle);
+    }
+
+    swarm->gbest = swarm->at(0)->current->clone();
+
+    return swarm;
+}
